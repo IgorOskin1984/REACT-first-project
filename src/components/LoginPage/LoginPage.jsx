8e65042d9cd355f7c0dc5cbfc300fa957146f55f
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-02-07 16:10:30 +0200

    урок 75 redux-form устарело

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
+
+const LoginForm = (props) => {
+
+	return (
+		<form onSubmit={props.handleSubmit} >
+			<div>
+				<Field component={'input'} name={'login'} type="text" />
+			</div>
+			<div>
+				<Field component={'input'} name={'password'} type="password" />
+			</div>
+			<div>
+				<Field component={'input'} name={'rememberMe'} type="checkbox" /> remember me
+			</div>
+			<div>
+				<button>login</button>
+			</div>
+		</form>
+	)
+}
+
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
 
 const LoginPage = (props) => {
+	let onSubmit = (formData) => {
+		console.log(formData)
+	}
 	return <div>
 		<h1>This is login page</h1>
+		<LoginReduxForm onSubmit={onSubmit} />
 	</div>
 }
 
