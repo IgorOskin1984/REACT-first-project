e1482dee6709fb169f4f4ecfe1edd84fd2af3ec3
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-02-17 10:40:42 +0200

    фиксация перед слиянием

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Footer from './components/Footer/Footer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import { Component } from 'react';
 import { connect } from 'react-redux';
-import {autoficationTC} from './redax/appReducer'
+import { autoficationTC } from './redax/appReducer'
 import { compose } from 'redux';
-import { withAuthRedirect } from './hoc/withAuthRedirectNavigate';
 import { withRouter } from './components/Main/Profile/ProfileContainer';
+import Preloader from './components/common/Preloader/Preloader';
 
 
 
@@ -21,21 +21,25 @@ class App extends Component {
 	}
 
 	render() {
-		return (<div className='wrapper'>
-			<HeaderContainer />
-			<Main />
-			<Footer />
-		</div>);
+		if (!this.props.initializedSuccess) {
+			return <Preloader/>
+		}
+			return (<div className='wrapper'>
+				<HeaderContainer />
+				<Main />
+				<Footer />
+			</div>);
 	}
 
 }
 
 const mapStateToProps = (state) => {
-	//initializedSuccess: state.app.initializedSuccess
+	return {
+		initializedSuccess: state.app.initializedSuccess
+	}
 }
 
 export default compose(
-	//withAuthRedirect,
 	withRouter,
-	connect(mapStateToProps, {autoficationTC})
+	connect(mapStateToProps, { autoficationTC })
 )(App);
