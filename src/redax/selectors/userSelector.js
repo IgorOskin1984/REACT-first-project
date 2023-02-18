61959201ad81810347822e8210132da333ea29ef
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-02-18 10:32:00 +0200

    83 написал createSelector

--- src/redax/selectors/userSelector.js
+++ src/redax/selectors/userSelector.js
@@ -1,6 +1,12 @@
-export const getUsers = (state) => {
+import { createSelector } from "reselect"
+
+const getUsersSimpleSelector = (state) => {
 	return state.usersPage.users
 }
+export const getUsers = createSelector(getUsersSimpleSelector, (users) => {
+	return users.filter(u => true);
+})
+
 export const getPageSize = (state) => {
 	return state.usersPage.pageSize
 }
