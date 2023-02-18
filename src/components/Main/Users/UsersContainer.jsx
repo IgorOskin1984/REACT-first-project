245b667b2ae366e6aedcb9a96f96092729103b5f
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-02-18 11:01:02 +0200

    83 написал селекторы сам

--- src/components/Main/Users/UsersContainer.jsx
+++ src/components/Main/Users/UsersContainer.jsx
@@ -11,6 +11,14 @@ import Users from './Users';
 import Preloader from '../../common/Preloader/Preloader';
 //import { withAuthRedirect } from '../../../hoc/withAuthRedirectNavigate';
 import { compose } from 'redux';
+import {
+	getCurrentPage,
+	getFollowingInProgress,
+	getIsFetching,
+	getPageSize,
+	getTotalUsersCount,
+	getUsers
+} from '../../../redax/selectors/usersSelectors';
 
 class UsersContainer extends React.Component {
 
@@ -44,12 +52,12 @@ class UsersContainer extends React.Component {
 
 let mapStateToProps = (state) => {
 	return {
-		users: state.usersPage.users,
-		pageSize: state.usersPage.pageSize,
-		totalUsersCount: state.usersPage.totalUsersCount,
-		currentPage: state.usersPage.currentPage,
-		isFetching: state.usersPage.isFetching,
-		followingInProgress: state.usersPage.followingInProgress,
+		users: getUsers(state),
+		pageSize: getPageSize(state),
+		totalUsersCount: getTotalUsersCount(state),
+		currentPage: getCurrentPage(state),
+		isFetching: getIsFetching(state),
+		followingInProgress: getFollowingInProgress(state),
 	}
 }
 //let mapDispatchToProps = (dispatch) => {
