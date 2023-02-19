40275084cc79bd01c0ffeff24b54842dfe17966a
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-02-19 10:30:02 +0200

    reselect, createSelector Установил reselect, создал селекторы. Использовал createSelector

--- src/components/Main/Users/UsersContainer.jsx
+++ src/components/Main/Users/UsersContainer.jsx
@@ -11,6 +11,7 @@ import Users from './Users';
 import Preloader from '../../common/Preloader/Preloader';
 //import { withAuthRedirect } from '../../../hoc/withAuthRedirectNavigate';
 import { compose } from 'redux';
+import { gePageSize, getCurrentPage, getFollowingInProgress, getIsFetching, getTotalUsersCount, getUsers } from '../../../redax/selectors/usersSelector';
 
 class UsersContainer extends React.Component {
 
@@ -44,12 +45,12 @@ class UsersContainer extends React.Component {
 
 let mapStateToProps = (state) => {
 	return {
-		users: state.usersPage.users,
-		pageSize: state.usersPage.pageSize,
-		totalUsersCount: state.usersPage.totalUsersCount,
-		currentPage: state.usersPage.currentPage,
-		isFetching: state.usersPage.isFetching,
-		followingInProgress: state.usersPage.followingInProgress,
+		users: getUsers(state),
+		pageSize: gePageSize(state),
+		totalUsersCount: getTotalUsersCount(state),
+		currentPage: getCurrentPage(state),
+		isFetching: getIsFetching(state),
+		followingInProgress: getFollowingInProgress(state)
 	}
 }
 //let mapDispatchToProps = (dispatch) => {
