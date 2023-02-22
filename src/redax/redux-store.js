ee97d93c4709351ad3358e9f3672ce13f6768c7a
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-02-22 13:58:13 +0200

    91  добавили расширения в хром
    расширения используем с новым пользоватилем без дополнительных плагинов

--- src/redax/redux-store.js
+++ src/redax/redux-store.js
@@ -1,11 +1,11 @@
-import { applyMiddleware, combineReducers, legacy_createStore as createStore } from "redux";
+import { applyMiddleware, compose, combineReducers, legacy_createStore as createStore } from "redux";
 import profilePageReducer from './profileReducer';
 import dialogsPageReducer from './dialogsReducer';
 import sidebarReducer from './sidebarReducer';
 import usersPageReducer from "./usersReducer";
 import authReducer from "./authReducer";
 import thunkMiddleware from 'redux-thunk';
-import {reducer as formReducer} from 'redux-form';
+import { reducer as formReducer } from 'redux-form';
 import { appReducer } from "./appReducer";
 
 
@@ -22,8 +22,12 @@ let reducers = combineReducers(
 
 	})
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
-window.store = store;
+	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+	const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+//let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+//redux-store без расширения в chrome
+window.__store__ = store;
 
 export default store;
 //src/redax/redux-store.js
