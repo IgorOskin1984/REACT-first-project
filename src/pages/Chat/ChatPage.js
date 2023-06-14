0ec7498579ce01754b8c5bb82edc75029e84c0ea
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-14 20:07:27 +0300

    Начинаю работу над ChatPage Димыч дробит ее над подкомпоненты, я делаю также. Делаю <Chat /> <Messages /> <Message /> -> <div>message</div>

--- src/pages/Chat/ChatPage.js
+++ src/pages/Chat/ChatPage.js
@@ -1,9 +1,28 @@
 const ChatPage = () => {
 	return <>
 		<div>
-			Chat
+			<Chat />
 		</div>
 	</>
 }
 
 export default ChatPage;
+
+const Chat = () => {
+	return <>
+		<div>
+			<Messages />
+		</div>
+	</>
+}
+
+const Messages = () => {
+	const arr = [1, 2, 3]
+	return arr.map((message) => {
+		return <Message />
+	})
+}
+
+const Message = () => {
+	return <div>message</div>
+}
